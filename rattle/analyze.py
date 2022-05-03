5d45cfb65fc9ed76e5c4f74e2c62a4ede5b9f9d9
CarstenNZ <8205404+CarstenNZ@users.noreply.github.com>
2022-05-04 07:08:13 +1200

    adds Block id, e.g. "BB 0x123" at the top of each ControlFlowGraph png block

--- rattle/analyze.py
+++ rattle/analyze.py
@@ -118,7 +118,7 @@ class ControlFlowGraph(object):
 
         for block in self.function:
             block_id = f'block_{block.offset:#x}'
-            block_body = '\\l'.join([f'{insn.offset:#x}: {insn}' for insn in block])
+            block_body = f'BB {block.offset:#x}\\l' + '\\l'.join([f'{insn.offset:#x}: {insn}' for insn in block])
             block_body = block_body.replace('<', '\\<').replace('>', '\\>')
             block_dot = f'{block_id} [label="{block_body}\\l", shape="record"];'
 
